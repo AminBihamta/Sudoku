d603158eac172373a9bae2e1a16366b0c7ca9fed
Amin <aminbihamtawork@gmail.com>
2023-09-04 06:27:13 -0700

    Implemented printBoard method

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 using namespace std;
 
+void printBoard(int board[9][9]) {
+
+	for (int i = 0; i < 9; i++)
+	{
+		if ((i) % 3 == 0 && i > 1)
+			cout << "---------------------" << endl;
+		for (int j = 0; j < 3; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+
+		cout << '|' << " ";
+
+		for (int j = 3; j < 6; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+
+		cout << '|' << " ";
+
+		for (int j = 6; j < 9; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+
+}
+
 int main()
 {
+	int board[9][9];
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			board[i][j] = NULL;
+		}
+	}
+
+	printBoard(board);
 }
